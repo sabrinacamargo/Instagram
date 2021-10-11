137feb0c05bd26c4eef57f6fc4ce65b411e6e188
Sabrina Camargo <scama030@fiu.edu>
2021-10-11 15:53:04 -0400

    Setting up pods and signup/login functionality

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+       
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "KcRELQvBvk5eLGDFGi0cOxsd9iRcg4XV7UW4YIEy" // <- UPDATE
+                    $0.clientKey = "N3dPxuU8d5ojJphL1Nm9S49wDhSh74Z56HX3vVXs" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -6,13 +6,36 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = self.usernameField.text
+        let password = self.passwordField.text
+        
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
 
